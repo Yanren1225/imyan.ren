6a1208d286ae56f56f0ed5cb86fa2ea8ddd3db80
Yanren <hi@imyan.ren>
2025-08-23 16:32:59 +0800

    开往

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,8 +9,20 @@ const Footer: Component = () => {
   return (
     <footer>
       <p>
-        © 2018 - {currentYear} {t('yanren')}
+        © 2018 - {currentYear} {t('yanren')}{' '}
       </p>
+      <a
+        href="https://www.travellings.cn/go.html"
+        target="_blank"
+        rel="noopener"
+        title="开往-友链接力"
+      >
+        <img
+          src="https://www.travellings.cn/assets/w.png"
+          alt="开往-友链接力"
+          width="100"
+        />
+      </a>
     </footer>
   )
 }
