4aee7210e24aef0c6932255f348f6962b5eb9bcf
Yanren <hi@imyan.ren>
2026-02-05 23:38:52 +0800

    fix: lock fix

--- website/dashboard.imyan.ren/uno.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'unocss'
-import { basePreset } from '@yanren/config'
-import transformerDirectives from '@unocss/transformer-directives'
-
-export default defineConfig({
-  presets: [basePreset()],
-  transformers: [transformerDirectives()],
-})
